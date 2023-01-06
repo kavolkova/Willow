45b09c713b7de0e21ff962f41bebe69768bca257
ezuckerman24 <elzuckerman@packer.edu>
2023-01-06 09:34:59 -0500

    changed house

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 df = pd.read_csv('/Users/lizuckerman/Desktop/ppsfs.csv')
 
 class House:
-  
+
   def __init__(self, owner, address, area, neighborhood, zipcode, beds, baths, washer_dryer, air_conditioning, heating, outdoor_space, parking, acreage, num_floors, type, furnished, energy_efficiency,storage_space, price = 0,likes=0):
     #ints
     self.price = int(price) #if no price is listed, will be 0 unti westimate is calculated
@@ -21,7 +21,7 @@ class House:
     self.neighborhood = neighborhood
     self.type= type
     self.energy_efficiency = energy_efficiency
-    
+
     #booleans
     self.washer_dryer = washer_dryer
     self.air_condintioning = air_conditioning
@@ -35,10 +35,10 @@ class House:
     # ppsf = df[i][1]
     # print(ppsf)
     #self.price = ppsf*self.area
-    #price += 
+    #price +=
 
 
-    
+
 class User():
 
   def __init__(self, username, password, type=""):
@@ -63,7 +63,7 @@ class User():
       pass
     else:
       pass
-    
-vanderbilt_414 = House.House('state', '414 Vanderbilt Ave', 500, 'Fort Greene', 11238, 4, 4, True, True, True, 50, False, 1, 4, 'House', False, 'D', 20)
+
+vanderbilt_414 = House('state', '414 Vanderbilt Ave', 500, 'Fort Greene', 11238, 4, 4, True, True, True, 50, False, 1, 4, 'House', False, 'D', 20)
 vanderbilt_414.westimate()
 print(vanderbilt_414.price)
