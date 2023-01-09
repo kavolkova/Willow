d28d0ec48bfe97badcb021cf3daa42e0b38dc693
kavolkova <120653147+kavolkova@users.noreply.github.com>
2023-01-09 12:03:26 -0500

    Update site_class.py

--- site_class.py
+++ site_class.py
@@ -1,3 +1,18 @@
+import main.py
+
 class Site():
-    def __init__():
-        
+    def __init__(self, accounts = {},houses = []):
+        self.accounts = accounts
+        self.houses = houses
+
+    def addAccount(self,username,password):
+        newaccount = main.User(username,password)
+        self.accounts[username] = password
+
+    def checklogin(self,username,password):
+        if username in self.accounts:
+            if password == self.accounts[username][0]:
+                return True
+        return False
+
+    def selectType()
